2fc2d6ac6d55c78fcf9c7c3bc67e88077df7c5da
inaba <software@panduro.guru>
2018-05-10 13:24:53 +0200

    aa

---
+++ app.js
@@ -0,0 +1,6 @@
+const express       = require("express");
+const handlebars    = require("express-handlebars");
+const path          = require("path");
+const bodyParser    = require("body-parser");
+const session       = require("express-session");
+const passport      = require("./helpers/passport");
