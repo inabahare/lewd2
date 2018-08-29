cb2d17f77ca940ad09780ec1908f46d03f08fe0b
inaba <software@panduro.guru>
2018-08-29 11:26:53 +0200

    Added and tested a queue for virustotal

---
+++ src/Functions/sleep.js
@@ -0,0 +1,9 @@
+/**
+ * Waits n amount of milliseconds in an awaitable manner
+ * @param {Number} milliseconds 
+ */
+const sleep = milliseconds => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+export default sleep;
--- src/cronactions.js
+++ src/cronactions.js
@@ -6,33 +6,57 @@ import getFilesToDelete         from "./Functions/FileDeletion/getFilesToDelete"
 import deleteFiles              from "./Functions/FileDeletion/deleteFiles";
 import getFilesForSecondaryScan from "./Functions/SecondaryScan/GetFilesForSecondaryScan";
 import getFileReport            from "./Functions/VirusTotal/getFileReport";
+import sleep                    from "./Functions/sleep";
 
 import dotenv from "dotenv";
 import markAsScannedTwice from './Functions/SecondaryScan/markAsScannedTwice';
 dotenv.config();
-
+/*
 setTimeout(async () => {
     console.log("Getting file report");
     const fileReport = await getFileReport("2546dcffc5ad854d4ddc64fbf056871cd5a00f2471cb7a5bfd4ac23b6e9eedad", process.env.VIRUSTOTAL_KEY);
     console.log(fileReport.positives);
     console.log("Report gotten");
 }, 1000);
-
+*/
 
 /**
  * Limits the AV scans
  */
-const queue = async.queue(async (task) => {
+const sophosQueue = async.queue(async (task) => {
     // Gets the filename if scan is positive fpr secondary scan
     const filename = await scanAndRemoveFile(task.fileName);
 }, 1);
+
+/**
+ * Virutotal scans
+ */
+let amountOfScans = 0;
+const virustotalQueue = async.queue(async task => {
+    amountOfScans++;
+
+
+    console.log(task.fileHash + amountOfScans);
+
+    if (amountOfScans == parseInt(process.env.VIRUSTOTAL_MAX_SCANS_PR_MINUTE)) {
+        console.log("Waiting");
+        await sleep(parseInt(60000));
+        amountOfScans = 0;
+    }
+}, 1);
+
+virustotalQueue.push({fileHash: "Hello World"});
+virustotalQueue.push({fileHash: "Hello World"});
+virustotalQueue.push({fileHash: "Hello World"});
+virustotalQueue.push({fileHash: "Hello World"});
+virustotalQueue.push({fileHash: "Hello World"});
 
 /**
  * Functions the app can call
  */
 const messageServer = dnode({
     scan: fileName => {
-        queue.push({
+        sophosQueue.push({
             fileName: fileName,
         });
     }
@@ -71,7 +95,7 @@ schedule(process.env.SECONDARY_SCAN_CRON, async () => {
 
 
     uniqueFilenames.forEach(fileName => {
-        queue.push({
+        sophosQueue.push({
             fileName: fileName
         });
     })
