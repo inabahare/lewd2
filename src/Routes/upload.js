add02a4beef69135d7a5532a9481014dd65a273c
inaba <jacob@panduro.guru>
2018-09-11 10:42:58 +0200

    Removed the old code

--- src/Routes/upload.js
+++ src/Routes/upload.js
@@ -44,6 +44,10 @@ const storageOptions = multer.diskStorage({
     filename:    (req, file, next) => next(null, renameFile(escape(file.originalname))) 
 });
 
+
+/**
+ * UPLOAD
+ */
 router.post("/", async (req, res) => {
     const uploader   = await getUploaderOrDefault(req.headers.token);
     
@@ -93,65 +97,5 @@ router.post("/", async (req, res) => {
         res.send(resultJson);
     });
 });
-
-/**
- * UPLOADn n
- */
-// router.post("/", async (req, res) => {
-//     const uploader   = await getUploaderOrDefault(req.headers.token);
-//     console.log(res.locals);
-
-//     const form       = new formidable.IncomingForm({
-//         uploadDir  :process.env.UPLOAD_DESTINATION,
-//         encoding   :"utf-8",
-//         hash       :"sha1",
-//         maxFileSize: uploader.uploadsize
-//     });
-
-//     form.parse(req, async (err, fields, file) => {
-//         if (err) {
-//             return res.status(400)
-//                       .send(err);
-//         }
-
-//         file.name = escape(file.name);
-
-//         // Check if filename is too long
-//         if (file.name.length > 200) {
-//             unlink(file.path);
-//             return res.status(400)
-//                       .send("The filename is too long");
-//         }
-
-//         file.originalName = file.name;
-//         file.deletionKey = deletionKey(10);
-
-//         // Check if the file exists
-//         const existingFileName = await getImageFilenameIfExists(file.hash);
-//         if (existingFileName) { // If file has been uploaded and not deleted
-//             updateExistingFile(file);
-//             file.name = existingFileName;
-//             file.duplicate = true;
-//         } 
-//         else { // If file doesn't exist or has been deleted
-//             file.duplicate = false;
-//             file.name = await renameFile(file.name);
-//             await rename(file.path, path.join(form.uploadDir, file.name));
-//             scanFile(file.name);
-//         }
-
-//         await addImageToDatabase(file, uploader.id);
-
-//         const resultJson = {
-//             "status": 200,
-//             "data": {
-//                 "link": process.env.UPLOAD_LINK + file.name,
-//                 "deleteionURL": process.env.SITE_LINK + "delete/" + file.deletionKey
-//             }
-//         };
-
-//         res.send(resultJson);
-//     });
-// });
 
 export default router;
