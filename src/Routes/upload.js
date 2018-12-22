fb57d3fd6e17b1221bdb894f1da596733f721124
kleiner <software@panduro.guru>
2018-12-23 00:39:22 +0100

    It should now not be possible to upload without being a memer

--- src/Routes/upload.js
+++ src/Routes/upload.js
@@ -3,6 +3,15 @@ import * as upload from "../Controllers/upload";
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+    if (res.locals.user.username == null) {
+        return res.status(403)
+                  .send("You need to be logged in to upload");
+    }
+
+    next();
+});
+
 router.post("/", upload.index.post);
 
 export default router;
