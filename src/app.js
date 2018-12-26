905ee01af501f2faf5d05f16d4dbd0276d0ed479
inaba <software@panduro.guru>
2018-12-26 23:41:00 +0100

    Moved the routes and templating engine to their own classes

--- src/app.js
+++ src/app.js
@@ -1,66 +1,30 @@
 "use strict";
 require('dotenv').config();
 
-import express       from "express";
+import express        from "express";
 import handlebars     from "express-handlebars";
 import path           from 'path';
 import flash          from "express-flash";
 import cookieSession  from "cookie-session";
 import bodyParser     from "body-parser";
 import cookieParser   from "cookie-parser";
+import fs             from "fs";
+import moment         from "moment"; 
 import frontEndError  from "./helpers/frontendErrorFormatter";
 import getUserDetails from './Functions/User/getUserDetails';
-import fs             from "fs";
 import passport       from "./helpers/passport";
-import moment         from "moment"; 
 
-// Routers
-import index    from "./Routes";
-import login    from "./Routes/login";
-import upload   from "./Routes/upload";
-import user     from "./Routes/user";
-import register from "./Routes/register";
-import deleter  from "./Routes/delete"; // God damn reserved keywo~~rds
-import admin    from "./Routes/admin";
+import { Routes } from "./config/app/Routes";
+import { Views } from "./config/app/Views";
 
 const randomNumber  = (x, y) =>  Math.floor((Math.random() * y) + x); 
 
 const app = express();
 
 // Load views
-app.engine ("hbs", handlebars ({ 
-    defaultLayout: "main",
-    extname: "hbs",
-    partialsDir: __dirname + "/../views/partials/",
-    helpers: {
-        partial: function (name) {
-            return name;
-        },
-        waifu: () => {
-            const files = fs.readdirSync(__dirname + "/../Public/Images/Waifus");
-            
-            if (files.length === 0)
-                return "";
+Views.SetEngine(app);
+Views.SetDetails(app);
 
-            const randomIndex = randomNumber(0, files.length);
-            return process.env.SITE_LINK + "Images/Waifus/" + files[randomIndex];
-        },
-        dateFormatter: function(date) {
-            return moment(date).format("LL");
-        }, 
-        typeFormatter: function(data) {
-            if (data.startsWith("https://") || data.startsWith("http://")) {
-                return `<a href="${data}">Link</a>`;
-            } else {
-                return data.split(`found in file ${process.env.UPLOAD_DESTINATION}`)[0]
-                           .split("Virus ")[1]
-                           .split("'")[1]
-            }
-        }
-    }
-}));
-app.set ("view engine", "hbs");
-app.set('views', path.join(__dirname, "/../views"));
 // app.enable('view cache');
 // Static files
 if (process.env.NODE_ENV === "development") {
@@ -109,19 +73,10 @@ app.use((req, res, next) => {
 });
 
 // Set the routes
-app.use("/",           index);
-app.use("/login",      login);
-app.use("/upload",     upload);
-app.use("/user",       user);
-app.use("/register",   register);
-app.use("/delete",     deleter);
-app.use("/user/admin", admin)
+Routes.SetPages(app);
 
 // 404
-app.use((req, res, next) =>{
-    res.status(404)
-       .render("404");
-});
+Routes.SetErrorPages(app);
 
 if (!fs.existsSync(process.env.UPLOAD_DESTINATION)) {
     console.error(`Could not open upload directory at: ${process.env.UPLOAD_DESTINATION}`);
---
+++ src/config/app/Routes.js
@@ -0,0 +1,29 @@
+import index    from "../../Routes";
+import login    from "../../Routes/login";
+import upload   from "../../Routes/upload";
+import user     from "../../Routes/user";
+import register from "../../Routes/register";
+import deleter  from "../../Routes/delete"; // God damn reserved keywo~~rds
+import admin    from "../../Routes/admin";
+
+
+class Routes {
+    static SetPages(app) {
+        app.use("/",           index);
+        app.use("/login",      login);
+        app.use("/upload",     upload);
+        app.use("/user",       user);
+        app.use("/register",   register);
+        app.use("/delete",     deleter);
+        app.use("/user/admin", admin)
+    }
+
+    static SetErrorPages(app) {
+        app.use((req, res, next) =>{
+            res.status(404)
+               .render("404");
+        });
+    }
+}
+
+export { Routes }
