e3531406247d3df307782988e1cedcaadc50bd7e
inaba <software@panduro.guru>
2018-12-31 00:14:27 +0100

    I am a genious

--- src/app/Controllers/index/sharex.js
+++ src/app/Controllers/index/sharex.js
@@ -3,7 +3,7 @@ function get(req, res) {
     const shareXConfig = `{
         "Name": "Local",
         "DestinationType": "ImageUploader, FileUploader",
-        "RequestURL": "${process.env.SITE_LINK}uploads",
+        "RequestURL": "${process.env.SITE_LINK}upload",
         "FileFormName": "file",
         "Headers": {
           "token": "${res.locals.user.token}"
