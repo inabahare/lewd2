536686e4a866c463e70f8ad02049d199eab5106c
inaba <software@panduro.guru>
2019-01-07 16:27:49 +0100

    There will now be neato stats pr. https://github.com/inabahare/lewd2/issues/33. Now I need to make a nice function to convert the bytes to whatever unit fits https://github.com/inabahare/lewd2/issues/38

--- src/app/Controllers/index/info.js
+++ src/app/Controllers/index/info.js
@@ -1,5 +1,15 @@
-function get(req, res) {
-    res.render("info");
+import { getStatistics } from "../../Functions/Info/getStatistics";
+import { getAmountOfDaysFilesAreStored } from "../../Functions/Info/getAmountOfDaysFilesAreStored";
+
+async function get(req, res) {
+    const days = getAmountOfDaysFilesAreStored();
+    const stats = await getStatistics(days);
+
+    console.log(stats);
+    
+    res.render("info", {
+        stats: stats
+    });
 }
 
 export { get };
---
+++ src/app/Functions/Info/getStatistics.js
@@ -0,0 +1,24 @@
+import { db } from "../../helpers/database";
+
+/**
+ * 
+ * @param {Number} daysFilesAreStored - To get the average uploads pr day
+ * @returns {object} {totalUploads, 
+ *                    averageFileCountPrDay, 
+ *                    uploadsFromToday,
+ *                    totalUploadSize}
+ */
+async function getStatistics(daysFilesAreStored) {
+    const client = await db.connect();
+    const query = await client.query(`SELECT 
+                                            COUNT("filesha") "totalUploads",
+                                            ROUND(COUNT(fileSha) / '${daysFilesAreStored}'::DECIMAL, 2) "averageFileCountPrDay",
+                                            (SELECT COUNT(fileSha) FROM "Uploads" WHERE "uploaddate" > CURRENT_DATE + interval '1h') "uploadsFromToday",
+                                            SUM("size") "totalUploadSize"
+                                        FROM "Uploads";`);
+    await client.release();
+
+    return query.rows[0];
+}
+
+export { getStatistics };
