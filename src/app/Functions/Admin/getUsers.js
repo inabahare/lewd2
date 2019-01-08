3885055875d42510c0cb980aefd1e5b201a2695a
inaba <software@panduro.guru>
2019-01-08 22:17:42 +0100

    /admin/view-users now show filecount as well https://github.com/inabahare/lewd2/issues/39. I've also made the table fullwidth :3

--- src/app/Functions/Admin/getUsers.js
+++ src/app/Functions/Admin/getUsers.js
@@ -1,13 +1,19 @@
 import { db } from "../../helpers/database";
 
+const sql = `
+    SELECT "Users".id, username, uploadsize, isadmin, COUNT("Uploads".filesha) "amountOfUploads"
+    FROM "Users"
+    LEFT JOIN "Uploads" ON "Users".id = "Uploads".userid
+    GROUP BY "Users".id, username, uploadsize, isadmin
+    ORDER BY "Users".id;
+`;
+
 /**
  * Gets all users except for the default one
  */
 const getAllUsers = async () => {
     const client = await db.connect();
-    const allUsers = await client.query(`SELECT id, username, uploadsize, isadmin 
-                                         FROM "Users" 
-                                         ORDER BY id ASC;`);
+    const allUsers = await client.query(sql);
     await client.release();
 
     return allUsers.rows;
