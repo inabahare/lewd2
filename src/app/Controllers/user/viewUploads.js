21b46bafbf82bcc86839d3085ec60bbcb273245d
inaba <software@panduro.guru>
2019-01-08 21:54:32 +0100

    Neato stats added to /view-uploads page https://github.com/inabahare/lewd2/issues/39

--- src/app/Controllers/user/viewUploads.js
+++ src/app/Controllers/user/viewUploads.js
@@ -3,17 +3,21 @@ import { getUploads } from "../../Functions/User/getUploads";
 
 async function get(req, res) {
     const uploads = await getUploads(res.locals.user.id);
+    let count = 0;
 
     // If there are dates then format them
     if (uploads) {
+        count = uploads.length;
         uploads.forEach(upload => {
-            upload.uploaddate = moment(upload.uploaddate).format("LL");
+            upload.uploaddate = moment(upload.uploaddate)
+                               .format("DD-MM-YYYY HH:mm:ss");
         });
     }
 
     res.render("user", {
         menuItem: "view-uploads",
-        uploads: uploads
+        uploads: uploads,
+        count: count
     });
 }
 
