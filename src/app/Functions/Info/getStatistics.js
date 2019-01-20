28a2b20891f9373a44eaae82329fd72ecdb92820
inaba <software@panduro.guru>
2019-01-20 19:59:31 +0100

    Just fixed an overflow that would cause the info page to crash :3

--- src/app/Functions/Info/getStatistics.js
+++ src/app/Functions/Info/getStatistics.js
@@ -14,9 +14,9 @@ async function getStatistics(daysFilesAreStored) {
     const client = await db.connect();
     const query = await client.query(`SELECT 
                                             COUNT("filesha")::INTEGER "totalUploads",
-                                            ROUND(COUNT(fileSha) / '${daysFilesAreStored}'::DECIMAL, 2) "averageFileCountPrDay",
-                                            (SELECT COUNT(fileSha) FROM "Uploads" WHERE "uploaddate" > CURRENT_DATE + interval '1h')::INTEGER "uploadsFromToday",
-                                            (SELECT COALESCE(SUM("size"), 0) FROM "Uploads" WHERE duplicate = FALSE)::INTEGER "totalUploadSize"
+                                            ROUND(COUNT(fileSha) / '${daysFilesAreStored}'::DECIMAL, 2)::TEXT "averageFileCountPrDay",
+                                            (SELECT COUNT(fileSha) FROM "Uploads" WHERE "uploaddate" > CURRENT_DATE + interval '1h')::TEXT "uploadsFromToday",
+                                            (SELECT COALESCE(SUM("size"), 0) FROM "Uploads" WHERE duplicate = FALSE)::TEXT "totalUploadSize"
                                         FROM "Uploads";`);
     await client.release();
 
