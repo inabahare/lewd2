a1be4c65e6e583951ee19d4b872a432d4deadf4c
inaba <software@panduro.guru>
2019-01-22 23:08:35 +0100

    Make way for the logging!

--- src/app/Controllers/upload/index.js
+++ src/app/Controllers/upload/index.js
@@ -8,8 +8,16 @@ import { HandleUpload } from "../../Classes/HandleUpload";
 
 // Pretty much just how shit needs to get stored
 const storageOptions = multer.diskStorage({
-    destination: (req, file, next) => next(null, process.env.UPLOAD_DESTINATION),
-    filename:    (req, file, next) => next(null, renameFile(escape(file.originalname))) 
+    destination: (req, file, next) => {
+        console.log("destination", file);
+        console.log("------------------------------");
+        next(null, process.env.UPLOAD_DESTINATION);
+    },
+    filename:    (req, file, next) => {
+        console.log("filename", file);
+        console.log("------------------------------");
+        next(null, renameFile(escape(file.originalname)));
+    } 
 });
 
 async function post(req, res) {
@@ -18,13 +26,16 @@ async function post(req, res) {
                   .send("You need to be signed in to upload");
     }
 
+    console.log("<file>");
+
     const uploader = await getUploader(req.headers.token);
     
     if (!uploader) {
         return res.status(400)
                   .send("You need to be signed in to upload");
     }
-
+    const multerStart = process.hrtime();
+    
     const upload = multer({ 
         storage: storageOptions,
         limits: {
@@ -32,10 +43,15 @@ async function post(req, res) {
         }
     }).single("file");
 
+    const multerStartTime = process.hrtime(multerStart);
+
+    const multerStartString = `It took ${multerStartTime} to start multer`;
+
     upload(req, res, async err => {
         if (err) {
             return HandleUpload.HandleError(uploader.uploadsize, res, err);
         }
+        const time = process.hrtime();
 
         const uploadHandler = new HandleUpload(req, res);
         await uploadHandler.AddHash();
@@ -46,7 +62,13 @@ async function post(req, res) {
         const resultJson = uploadHandler.GenerateResultJson(process.env.UPLOAD_LINK, process.env.SITE_LINK);
         uploadHandler.HandleSuccess(resultJson);
         
+        const timeToUpload = process.hrtime(time)[1];
 
+        console.log("<time>");
+        console.log(`It took ${timeToUpload} nanoseconds to upload the file: <i>${req.file.filename}</i>`);
+        console.log(multerStartString);
+        console.log("</time>");
+        console.log("</file>");
         // await Upload(req, res, uploader, err);
     });
 }
