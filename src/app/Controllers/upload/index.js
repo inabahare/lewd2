78ff9c24fcd492d6959610f45bf9db7631b2a477
inaba <software@panduro.guru>
2019-01-24 11:56:13 +0100

    Removed stat log thing

--- src/app/Controllers/upload/index.js
+++ src/app/Controllers/upload/index.js
@@ -22,7 +22,7 @@ async function post(req, res) {
                   .send("You need to be signed in to upload");
     }
 
-    console.log("<file>");
+    const uploadTimer = process.hrtime();
 
     const uploader = await getUploader(req.headers.token);
     
@@ -30,7 +30,6 @@ async function post(req, res) {
         return res.status(400)
                   .send("You need to be signed in to upload");
     }
-    const multerStart = process.hrtime();
 
     const upload = multer({ 
         storage: storageOptions,
@@ -39,56 +38,21 @@ async function post(req, res) {
         }
     }).single("file");
 
-    const multerStartTime = process.hrtime(multerStart);
-
-    const multerStartString = `It took ${multerStartTime} to start multer`;
-
     upload(req, res, async err => {
         if (err) {
             return HandleUpload.HandleError(uploader.uploadsize, res, err);
         }
-        let time = process.hrtime();
-        const totalTime = process.hrtime();
-
-        console.log(`Starting upload handler`);
+        
         const uploadHandler = new HandleUpload(req, res);
-        console.log(`Uploader took ${process.hrtime(time)[1]} nanoseconds\n`);
-
-        time = process.hrtime();
-
-        console.log(`Checking for existing file`);
         await uploadHandler.HandleExistingFile();
-        console.log(`File check took ${process.hrtime(time)[1]} nanoseconds\n`);
-
-        time = process.hrtime();
-
-        console.log(`Adding deletion key`);
-        uploadHandler.AddDeletionKey();
-        console.log(`Deletionkey took ${process.hrtime(time)[1]} nanoseconds\n`);
-
-        time = process.hrtime();
-
-        console.log(`Adding to database`);
+              uploadHandler.AddDeletionKey();
         await uploadHandler.AddImageToDatabase(uploader.id);
-        console.log(`Adding to database took ${process.hrtime(time)[1]} nanoseconds\n`);
 
-        time = process.hrtime();
-
-        console.log(`Sending result`);
         const resultJson = uploadHandler.GenerateResultJson(process.env.UPLOAD_LINK, process.env.SITE_LINK);
         uploadHandler.HandleSuccess(resultJson);
-        console.log(`Result took ${process.hrtime(time)[1]} nanoseconds\n`);
 
-        time = process.hrtime();
-
-        const timeToUpload = process.hrtime(totalTime)[1];
-
-        console.log("<time>");
-        console.log(`It took ${timeToUpload} nanoseconds to upload the file: <i>${req.file.filename}</i>`);
-        console.log(multerStartString);
-        console.log("</time>");
-        console.log("</file>");
-        // await Upload(req, res, uploader, err);
+        const uploadTime = process.hrtime(uploadTimer);
+        console.log(`It took ${uploadTime[1]} nanoseconds to process ${req.file.filename}`);
     });
 }
 
