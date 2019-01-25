e18d82e31a342fd48358e98635f3dc6b6c432cbf
inaba <software@panduro.guru>
2019-01-25 12:04:15 +0100

    Removed the current schedules from the cronactions so that I can get my shit together and do something about this shit

--- src/cronactions.js
+++ src/cronactions.js
@@ -3,10 +3,6 @@ dotenv.config();
 
 import { schedule }             from "node-cron";
 import dnode                    from "dnode";
-import async                    from "async"; 
-import scanAndRemoveFile        from "./AntivirusApi/Functions/scanAndRemoveFile";
-import getFilesForSecondaryScan from "./AntivirusApi/Functions/SecondaryScan/GetFilesForSecondaryScan";
-import getFilesToScan           from "./AntivirusApi/Functions/VirusTotal/getFilesToScan";
 import VirusTotalScanner        from "./AntivirusApi/Classes/VirusTotalScanner";
 
 import { fileDeletion } from "./AntivirusApi/Controllers/Cron/fileDeletion";
@@ -19,22 +15,9 @@ const virusTotal = new VirusTotalScanner(process.env.VIRUSTOTAL_KEY,
 
 
 /**
- * Limits the AV scans
- */
-const sophosQueue = async.queue(async (task) => {
-    await scanAndRemoveFile(task.fileName, task.fileSha);
-}, 1);
-
-/**
  * Functions the app can call
  */
 const messageServer = dnode({
-    sophosScan: (fileName, fileSha) => {
-        sophosQueue.push({
-            fileName: fileName,
-            fileSha: fileSha
-        });
-    },
     virusTotalScan: (fileHash, fileName, scanNumber) => {
         virusTotal.scan(fileHash, fileName, scanNumber);
     }
@@ -47,49 +30,7 @@ const messageServer = dnode({
  */
 schedule(process.env.FILE_DELETION_CRON, fileDeletion);
 
-/**
- * Secondary AV scan of uploaded files
- */
-schedule(process.env.SECONDARY_SCAN_CRON, async () => {
-    const files = await getFilesForSecondaryScan();
 
-    if (!files) {
-        return;
-    }
-
-    if (files.length === 0) {
-        return;
-    }
-
-    console.log(`Performing secondaty scan on ${files.length + 1} files`);
-
-    files.forEach(file => {
-        sophosQueue.push({
-            fileName: file.fileName,
-            fileSha: file.filesha
-        });
-    });
-});
-
-schedule(process.env.VIRUSTOTAL_SECOND_AND_THIRD_SCAN_CRON, async () => {
-    const files = await getFilesToScan();
-
-
-    if (!files) {
-        return;
-    }
-
-    if (files.length === 0) {
-        return;
-    }
-    
-    console.log(`Performing tertiary scan on ${files.length + 1} files`);
-
-
-    files.forEach(file => {
-        virusTotal.scan(file.filehash, file.filename, ++file.virusTotalScan);
-    });    
-});
  
 // Catch all exceptions in production mode
 if (process.env.NODE_ENV === "production") {
