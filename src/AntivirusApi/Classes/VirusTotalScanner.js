e44561203c29feb83c6d3ac501fe1868d95eb110
inaba <software@panduro.guru>
2019-01-28 16:27:54 +0100

    The virustotal class now also does stuff when file is clean or doesn't exist

--- src/AntivirusApi/Classes/VirusTotalScanner.js
+++ src/AntivirusApi/Classes/VirusTotalScanner.js
@@ -6,7 +6,7 @@ import { logToTransparency } from "../Functions/Transparency/logToTransparency";
 const uploadDestination   = process.env.UPLOAD_DESTINATION;
 const minAllowedPositives = parseInt(process.env.VIRUSTOTAL_MIN_ALLOWED_POSITIVES);
 const apiKey              = process.env.VIRUSTOTAL_KEY;
-const maxScansPrDay       = parseInt(process.env.VIRUSTOTAL_MAX_SCANS_PR_DAY);
+// const maxScansPrDay       = parseInt(process.env.VIRUSTOTAL_MAX_SCANS_PR_DAY);
 
 const virusTotal = new VirusTotal(apiKey);
 
@@ -24,7 +24,6 @@ class VirusTotalScanner {
         if (!fileHash) {
             throw new Error("Filehash not provided for virustotal scanner");
         }
-        
         virusTotal.scan(fileHash);
     }
     
@@ -38,15 +37,27 @@ class VirusTotalScanner {
     async _onScanDone(virusTotalData) {
         if (virusTotalData.positives >= minAllowedPositives) {
             await VirusTotalScanner._handleVirus(virusTotalData);
-            return;
+        }
+        else {
+            await VirusTotalScanner._handleClean(virusTotalData);
         }
     }
 
-    static async _handleVirus(virusTotalData) {
-        const fileName = await VirusTotalScanner._getFilenameByHash(virusTotalData.sha256);
-        await deleteFilesByFileHash(virusTotalData.sha256, uploadDestination);
-        await logToTransparency(fileName, virusTotalData.sha256, virusTotalData.permalink, "VirusTotal");
+    static async _handleClean(virusTotalData) {
+        const client = await db.connect();
+        await client.query(`UPDATE "Uploads"
+                            SET "virustotalScan" = "virustotalScan" + 1
+                            WHERE filesha = $1;`, [ virusTotalData.resource ]);
+        await client.release();
     }
+
+    static async _handleVirus(virusTotalData) {
+        const fileName = await VirusTotalScanner._getFilenameByHash(virusTotalData.resource);
+        await deleteFilesByFileHash(virusTotalData.resource, uploadDestination);
+        await logToTransparency(fileName, virusTotalData.resource, virusTotalData.permalink, "VirusTotal");
+    }
+
+
 
     static async _getFilenameByHash(fileHash) {
         const client = await db.connect();
@@ -56,7 +67,7 @@ class VirusTotalScanner {
         if(result.rows.length === 0) 
             return null;
         
-        return result.rows[0];
+        return result.rows[0].filename;
     }
 //#endregion
 }
