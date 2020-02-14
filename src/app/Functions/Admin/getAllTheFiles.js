2ed064d97cb5ab887b73bf25ef0aad25846a570e
inaba <software@panduro.guru>
2020-02-14 13:36:17 +0100

    The file getter

--- src/app/Functions/Admin/getAllTheFiles.js
+++ src/app/Functions/Admin/getAllTheFiles.js
@@ -1,15 +1,12 @@
-import { db } from "../../helpers/database";
+import { query } from "../../Functions/database";
 
 async function getUploads() {
-    const client = await db.connect();
+    const uploads = await query(`SELECT filename, size, originalname, uploaddate, duplicate, virus, passworded, deletionkey, "Users".username  
+                                 FROM "Uploads", "Users"
+                                 WHERE "Uploads".userid = "Users".id
+                                 ORDER BY uploaddate DESC`);
 
-    const uploads = await client.query(`SELECT filename, size, originalname, uploaddate, duplicate, virus, passworded, deletionkey, "Users".username  
-                                        FROM "Uploads", "Users"
-                                        WHERE "Uploads".userid = "Users".id
-                                        ORDER BY uploaddate DESC`);
-    await client.release();
-
-    return uploads.rows;
+    return uploads;
 }
 
 export { getUploads };
