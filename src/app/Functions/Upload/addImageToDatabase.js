107b2e30f4fd301152142ccbbebf3abd81f1a0c1
inaba <software@panduro.guru>
2020-02-14 15:52:41 +0100

    AddImage

--- src/app/Functions/Upload/addImageToDatabase.js
+++ src/app/Functions/Upload/addImageToDatabase.js
@@ -1,4 +1,4 @@
-import { db }          from "../../helpers/database";
+import { query } from "../../Functions/database";
 import deletionKey from "./deletionKey";
 
 /**
@@ -7,30 +7,26 @@ import deletionKey from "./deletionKey";
  * @param {number} userid 
  */
 const addImageToDatabase = async (file, userid) => {
-    const client = await db.connect();
-
     // Check for unique deletion key
     file.deletionKey = deletionKey();
 
+    const data = [
+        file.filename,
+        file.originalname, 
+        file.hash, 
+        userid, 
+        file.duplicate.toString(),
+        file.deletionKey,
+        file.size
+    ];
+
     try {
-        await client.query(`INSERT INTO "Uploads" (filename, originalName, filesha, userid, duplicate, uploaddate, deletionkey, size) 
-                            VALUES ($1, $2, $3, $4, $5, NOW(), $6, $7);`, [
-                                file.filename,
-                                file.originalname, 
-                                file.hash, 
-                                userid, 
-                                file.duplicate.toString(),
-                                file.deletionKey,
-                                file.size
-                            ]);
+        await query(`INSERT INTO "Uploads" (filename, originalName, filesha, userid, duplicate, uploaddate, deletionkey, size) 
+                     VALUES ($1, $2, $3, $4, $5, NOW(), $6, $7);`, data);
     }
     catch(ex) {
         console.error(`Failed to insert file to database with error: ${ex.message}`);
     }
-    finally {
-        await client.release();
-    }
-
 };
 
 export default addImageToDatabase;
