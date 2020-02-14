f11e640d4a9d7ad09f8e5e7454eb62b4ac228725
inaba <software@panduro.guru>
2020-02-14 15:48:32 +0100

    stats

--- src/app/Functions/Info/getStatistics.js
+++ src/app/Functions/Info/getStatistics.js
@@ -1,4 +1,4 @@
-import { db } from "../../helpers/database";
+import { query } from "../../Functions/database";
 import { convertNumberToBestByteUnit } from "../convertNumberToBestByteUnit";
 
 
@@ -11,16 +11,14 @@ import { convertNumberToBestByteUnit } from "../convertNumberToBestByteUnit";
  *                    totalUploadSize}
  */
 async function getStatistics(daysFilesAreStored) {
-    const client = await db.connect();
-    const query = await client.query(`SELECT 
-                                            COUNT("filesha")::INTEGER "totalUploads",
-                                            ROUND(COUNT(fileSha) / '${daysFilesAreStored}'::DECIMAL, 2)::TEXT "averageFileCountPrDay",
-                                            (SELECT COUNT(fileSha) FROM "Uploads" WHERE "uploaddate" > CURRENT_DATE + interval '1h')::TEXT "uploadsFromToday",
-                                            (SELECT COALESCE(SUM("size"), 0) FROM "Uploads" WHERE duplicate = FALSE)::TEXT "totalUploadSize"
-                                        FROM "Uploads";`);
-    await client.release();
+    const stats = await query(`SELECT 
+                                COUNT("filesha")::INTEGER "totalUploads",
+                                ROUND(COUNT(fileSha) / '${daysFilesAreStored}'::DECIMAL, 2)::TEXT "averageFileCountPrDay",
+                                (SELECT COUNT(fileSha) FROM "Uploads" WHERE "uploaddate" > CURRENT_DATE + interval '1h')::TEXT "uploadsFromToday",
+                                (SELECT COALESCE(SUM("size"), 0) FROM "Uploads" WHERE duplicate = FALSE)::TEXT "totalUploadSize"
+                               FROM "Uploads";`);
 
-    const result = query.rows[0];
+    const result = stats[0];
 
     // when there is nothing to do statistics on
     if (result.totalUploads === 0) {
