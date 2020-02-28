d25a8207d9ffc8aa534e11cbf1b4281a9dd1c9d7
inaba <software@panduro.guru>
2020-02-28 15:41:01 +0100

    I don't think these limits makes much sense

--- src/app/Controllers/admin/token.js
+++ src/app/Controllers/admin/token.js
@@ -40,7 +40,7 @@ const validate = [
                  .isNumeric().withMessage("Upload size must be a number")
                  .isInt({
                     gt: 1,
-                    lt: 999999999999
+                    lt: 9999
                  }).withMessage("Upload size can't be less than 2 bytes or greater than 1TB (0.91TiB)"),
 
     check("unit").isLength({ min: 1, max: 3 }).withMessage("Upload unit needs to be a valid unit")
