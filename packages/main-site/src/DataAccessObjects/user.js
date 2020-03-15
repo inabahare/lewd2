9e2bce203a842921786b605d07cf805ad7af91a8
inaba <software@panduro.guru>
2020-03-16 00:24:51 +0100

    Added the needed DAO for user creation

---
+++ packages/main-site/src/DataAccessObjects/user.js
@@ -0,0 +1,41 @@
+import { query } from "/Functions/database";
+import { v1 as uuidv1 } from "uuid";
+import bcrypt from "bcrypt";
+
+export class User {
+  static async CheckIfUserExists (username) {
+    if (!username) {
+      throw Error ("Username needs to be set");
+    }
+
+    const user = await query(`SELECT username FROM "Users" WHERE username = $1;`, [ username ]);
+
+    if (!user) {
+      return false;
+    }
+
+    return user.length === 1;
+  }
+  
+  static async Create (args) {
+    const { username, password, uploadSize, isAdmin} = args;
+
+    if (!username || !password || !uploadSize) {
+      throw Error("The user's username, password, and upload size cannot be blank");
+    }
+
+    const passwordHashed = await bcrypt.hash(password, parseInt(process.env.BCRYPT_SALT_ROUNDS));
+    const token = uuidv1();
+
+    const data = [
+      username,
+      passwordHashed,
+      token,
+      uploadSize,
+      isAdmin // TODO: Should probably check for this
+    ];
+
+    await query(`INSERT INTO "Users" (username, password, token, uploadsize, isadmin)
+                 VALUES ($1, $2, $3, $4, $5);`, data);
+  }
+}
