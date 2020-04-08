32fa970f2a660f83f52834d1799f19492787bb60
inaba <software@panduro.guru>
2020-04-09 00:45:03 +0200

    2 spaces

--- packages/main-site/src/Controllers/admin/deleteFiles.js
+++ packages/main-site/src/Controllers/admin/deleteFiles.js
@@ -4,30 +4,30 @@ import { Uploads } from "/DataAccessObjects";
 
 
 function get(req, res) {
-    res.render("user", {
-        menuItem: "remove-files"
-    });
+  res.render("user", {
+    menuItem: "remove-files"
+  });
 }
 
 async function post(req, res) {
-    const linkArray = req.body.theLinks.split("\r\n");
+  const linkArray = req.body.theLinks.split("\r\n");
 
-    if (!linkArray[0]) {
-        return;
+  if (!linkArray[0]) {
+    return;
+  }
+
+  // Get array of filenames from link array
+  const fileNames = linkArray.map(l => l.replace(process.env.UPLOAD_LINK, ""));
+
+  for (const fileName of fileNames) {
+    const fullPath = Uploads.GetFullPath(fileName);
+    if (fs.existsSync(fullPath)) {
+      const deleteData = await Uploads.DeleteFile (fileName);
+      await logToTransparency(fileName, deleteData[0].filesha, "Google/Katt does not approve", "Google/Katt"); // TODO: Something about htis
     }
+  }
 
-    // Get array of filenames from link array
-    const fileNames = linkArray.map(l => l.replace(process.env.UPLOAD_LINK, ""));
-
-    for (const fileName of fileNames) {
-        const fullPath = Uploads.GetFullPath(fileName);
-        if (fs.existsSync(fullPath)) {
-            const deleteData = await Uploads.DeleteFile (fileName);
-            await logToTransparency(fileName, deleteData[0].filesha, "Google/Katt does not approve", "Google/Katt"); // TODO: Something about htis
-        }
-    }
-
-    res.redirect("/user/admin/remove-files");
+  res.redirect("/user/admin/remove-files");
 }
 
 const validate = [
