d6e8a58bb8b05f0c9941b6771ca62618d361e445
inaba <software@panduro.guru>
2020-07-25 01:16:34 +0200

    Copied the upload size formatter

---
+++ packages/discord/src/functions/formatUploadSize.js
@@ -0,0 +1,16 @@
+export const units = {
+  B: 1,
+  kB: 1000,
+  MB: 1000000,
+  GB: 1000000000,
+  KiB: 1024,
+  MiB: 1048576,
+  GiB: 1073741824
+};
+/**
+* Returns the input size in bytes according to the unit
+* @param {number} size 
+* @param {string} unit 
+*/
+export const formatUploadSize = 
+  (size, unit) => parseInt(size) * units[unit];
