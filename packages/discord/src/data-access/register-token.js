ec93a2befba8f3409108d6740e8c851b1d0f0403
inaba <software@panduro.guru>
2020-07-25 11:20:39 +0200

    Discord id on registergoken

--- packages/discord/src/data-access/register-token.js
+++ packages/discord/src/data-access/register-token.js
@@ -11,16 +11,17 @@ export class RegisterToken {
   static async Add (args) {
     const { uploadSize, isAdmin, discordId} = args;
     const registerToken = this.CreateToken();
-    
+
     const data = [
       registerToken, 
       uploadSize, 
-      isAdmin
+      isAdmin,
+      discordId
     ];
 
     const sql =
-      `INSERT INTO "RegisterTokens" (token, registered, uploadsize, isadmin)
-      VALUES ($1, NOW(), $2, $3);`;
+      `INSERT INTO "RegisterTokens" (token, registered, uploadsize, isadmin, "discordId")
+      VALUES ($1, NOW(), $2, $3, $4);`;
 
     await query(sql, data);
     
