e18205c0c4cbf3684f3469c744a5058bdd217927
inaba <software@panduro.guru>
2020-07-29 00:36:41 +0200

    Reset link now generated

--- packages/discord/src/commands/forgotPassword.js
+++ packages/discord/src/commands/forgotPassword.js
@@ -1,4 +1,8 @@
 import { PasswordToken } from "/data-access/password-token";
+import { User } from "/data-access";
+import { sendMessage } from "/functions/discord";
+
+const { SITE_LINK } = process.env;
 
 /**
  * 
@@ -7,6 +11,15 @@ import { PasswordToken } from "/data-access/password-token";
  * @param { Discord.Client } client 
  */
 export const forgotPassword =
-  (args, message, client) => {
+  async (args, message, client) => {
+    const { id } = message.author;
+    const user = await User.FindByDiscordId(id);
 
+    if (!user) 
+      return; // TODO: Add message here
+
+    const key = await PasswordToken.GenerateKey(user.username);
+    const link = `${SITE_LINK}login/forgot-password/${key}`;
+
+    sendMessage(message.author, `Hey bro here's your reset link: ${link}`);
   };
--- packages/discord/src/data-access/user.js
+++ packages/discord/src/data-access/user.js
@@ -2,7 +2,7 @@ import { query } from "/functions/query";
 
 export class User {
   static async FindByDiscordId(discordId) {
-    const sql = `SELECT id FROM "Users" WHERE "discordId" = $1`;
+    const sql = `SELECT id, username  FROM "Users" WHERE "discordId" = $1`;
     const data = [ discordId ];
 
     return query(sql, data);
