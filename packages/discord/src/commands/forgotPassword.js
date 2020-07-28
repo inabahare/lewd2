f1cb49c3b127551408ff0f778ef0ff588bdece89
inaba <software@panduro.guru>
2020-07-29 00:17:21 +0200

    Finding userId by discordid

--- packages/discord/src/commands/forgotPassword.js
+++ packages/discord/src/commands/forgotPassword.js
@@ -1,4 +1,12 @@
+import { PasswordToken } from "/data-access/password-token";
+
+/**
+ * 
+ * @param { array } args 
+ * @param { Discord.Message } message 
+ * @param { Discord.Client } client 
+ */
 export const forgotPassword =
   (args, message, client) => {
-    
+
   };
--- packages/discord/src/data-access/user.js
+++ packages/discord/src/data-access/user.js
@@ -2,6 +2,9 @@ import { query } from "/functions/query";
 
 export class User {
   static async FindByDiscordId(discordId) {
+    const sql = `SELECT id FROM "Users" WHERE "discordId" = $1`;
+    const data = [ discordId ];
 
+    return query(sql, data);
   }
 }
