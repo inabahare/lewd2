7625b3409e34b12a93067e26336515880391cf2e
inaba <software@panduro.guru>
2020-08-24 01:32:55 +0200

    Grpc? More like POST request amiriteguysssss

--- packages/backend/src/Functions/Upload/scan.js
+++ packages/backend/src/Functions/Upload/scan.js
@@ -1,20 +1,57 @@
-import dnode from "dnode";
+import http from "http";
+
+const post = async (endpoint, data) =>
+  new Promise((resolve, reject) => {
+    const postDataJson = JSON.stringify(data);
+    const port = parseInt(MESSAGE_SERVER_PORT);
+
+    const options = {
+      host: `localhost`, // Why can't
+      port: port,        // you
+      protocol: "http:", // just 
+      path: "/scan",     // be normal
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Length": postDataJson.length
+      },
+    };
+    
+    const request = http.request(options, result => {
+      result.on("end", () => {
+        resolve();
+      });
+    });
+
+    request.on("error", error => {
+      reject(`Error posting ${postDataJson}, ${error.message}`);
+    });
+
+    request.write(postDataJson);
+
+    request.end();
+  });
+
+const {
+  MESSAGE_SERVER_PORT
+} = process.env;
 
 /**
  * Scans a file with sophos and gets a file report from VirusTotal
  */
-const scan = (fileName, fileHash) => {
-  const external = dnode.connect(parseInt(process.env.MESSAGE_SERVER_PORT));
+const scan = async (fileName, fileHash) => {
+  const body = {
+    fileName, fileHash
+  };
 
-  external.on("remote", remote => {
-    remote.scan(fileName, fileHash);
-    external.end();
-  });
+  const servicesUrl = `localhost:${MESSAGE_SERVER_PORT}`;
 
-  external.on("error", function (err) {
-    console.error(err);
-    external.end();
-  }); 
+  try {
+    await post(servicesUrl, body);
+  } catch (e) {
+    console.error("Could not connect to services server");
+    console.error(e);
+  }
 };
 
 export default scan;
