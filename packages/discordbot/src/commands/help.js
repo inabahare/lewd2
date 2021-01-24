f4b77ae1a2218e08d0ac4b70a836a106e174b0b2
inaba <software@panduro.guru>
2021-01-24 20:15:59 +0100

    More possible uncursing

--- packages/discordbot/src/commands/help.js
+++ packages/discordbot/src/commands/help.js
@@ -1,14 +1,10 @@
 import { commands } from "../bot-commands";
 
-const {
-  COMMAND_PREFIX
-} = process.env;
-
 // `\n${COMMAND_PREFIX}${current.command} ${current.docs}`
 
 export function printHelp(args, message) {
   const reducer =
-    (result, current) => result += current.docs ? `\n${COMMAND_PREFIX}${current.command} ${current.docs}` : "";
+    (result, current) => result += current.docs ? `\n${process.env.COMMAND_PREFIX}${current.command} ${current.docs}` : "";
 
   const resultMessage =
     commands.reduce(reducer, "");
