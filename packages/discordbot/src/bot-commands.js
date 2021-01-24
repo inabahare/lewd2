0d80f32a5e27f71070a76804f12c3c28257f87f0
inaba <software@panduro.guru>
2021-01-24 18:18:49 +0100

    Ducks

--- packages/discordbot/src/bot-commands.js
+++ packages/discordbot/src/bot-commands.js
@@ -8,7 +8,8 @@ import {
 
 const {
   BOT_CHANNEL,
-  APPLICATIONS_CHANNEL
+  APPLICATIONS_CHANNEL,
+  SITE_NAME
 } = process.env;
 
 export const commands = [
@@ -16,30 +17,35 @@ export const commands = [
     command: "apply",
     action: apply,
     channel: BOT_CHANNEL,
-    minArgs: 0
+    minArgs: 0,
+    docs: "Will start the application process for you"
   },
   {
     command: "forgot-password",
     action: forgotPassword,
     channel: BOT_CHANNEL,
-    minArgs: 1
+    minArgs: 1,
+    docs: "Will send you a password reset link"
   },
   {
     command: "accept",
     action: accept,
     channel: APPLICATIONS_CHANNEL,
-    minArgs: 1
+    minArgs: 1,
+    docs: "Sends the quoted user an accept link and an optional message"
   },
   {
     command: "reject",
     action: reject,
     channel: APPLICATIONS_CHANNEL,
-    minArgs: 1
+    minArgs: 1,
+    docs: "Like accept but with a reject link"
   },
   {
     command: "add-me",
     action: integrate,
     channel: "dm",
-    args: 1
+    args: 1,
+    docs: `<YOUR UPLOAD TOKEN> - Adds your discord ID to your ${SITE_NAME} account so you for example can request a new password`
   }
 ];
