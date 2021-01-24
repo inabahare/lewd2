9b32200eb51d86760393d165e7e4bc8a2b0f9f5c
inaba <software@panduro.guru>
2021-01-24 19:09:36 +0100

    Newline error

--- packages/discordbot/src/index.js
+++ packages/discordbot/src/index.js
@@ -63,7 +63,7 @@ client.on("ready", () => {
   if (!applicationsChannel) channels.push(`APPLICATIONS_CHANNEL (${APPLICATIONS_CHANNEL}) not found`);
   if (!COMMAND_PREFIX) channels.push(`COMMAND_PREFIX not found. Possibly not set in .env`);
 
-  const error = channels.join(" & ");
+  const error = channels.join("\n");
 
   if (channels.lengt > 0) {
     console.error(error);
